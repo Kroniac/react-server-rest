b5d22759126793970b6b5d2e97628d7221d19bdf
Farid Ansari <farid12ansari7@gmail.com>
2017-11-08 00:50:45 +0530

    showing title,author from server

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,19 +7,30 @@ import NewPost from "../../components/NewPost/NewPost";
 import "./Blog.css";
 
 class Blog extends Component {
+  state = {
+    post: []
+  };
+
   componentWillMount() {
     axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
-      console.log(response);
+      const posts = response.data.slice(0, 4);
+      const updatedPosts = posts.map(post => {
+        return {
+          ...post,
+          author: "Farid Ansari"
+        };
+      });
+      this.setState({ post: updatedPosts });
+      //console.log(response);
     });
   }
   render() {
+    const posts = this.state.post.map(post => {
+      return <Post key={post.id} title={post.title} author={post.author} />;
+    });
     return (
       <div>
-        <section className="Posts">
-          <Post />
-          <Post />
-          <Post />
-        </section>
+        <section className="Posts">{posts}</section>
         <section>
           <FullPost />
         </section>
