3c83beaa88dd0d60942699f19a369d601274d8f3
Farid Ansari <farid12ansari7@gmail.com>
2017-11-08 06:36:59 +0530

    setting global header configuration for axios

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -13,7 +13,7 @@ class FullPost extends Component {
         (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)
       ) {
         axios
-          .get("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
+          .get("posts/" + this.props.id)
           .then(response => {
             this.setState({ loadedPost: response.data });
           });
@@ -23,7 +23,7 @@ class FullPost extends Component {
 
   deletePostHander = () => {
     axios
-      .delete("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
+      .delete("posts/" + this.props.id)
       .then(response => {
         console.log(response);
       });
--- src/index.js
+++ src/index.js
@@ -5,22 +5,32 @@ import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import axios from "axios";
 
-axios.interceptors.request.use(request => {
-  console.log(request);
-  //here i can edit request config
-  return request;
-}, error => {
+axios.defaults.baseURL = "https://jsonplaceholder.typicode.com/";
+axios.defaults.headers.common["Authorization"] = "AUTh TOKEn";
+axios.defaults.headers.post["Content-Type"] = "application/json";
+
+axios.interceptors.request.use(
+  request => {
+    console.log(request);
+    //here i can edit request config
+    return request;
+  },
+  error => {
     console.log(error);
     return Promise.reject(error);
-});
+  }
+);
 
-axios.interceptors.response.use(response => {
+axios.interceptors.response.use(
+  response => {
     console.log(response);
     //here i can edit request config
     return response;
-  }, error => {
-      console.log(error);
-      return Promise.reject(error);
-  });
+  },
+  error => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
